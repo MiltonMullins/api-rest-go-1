de5c9f76f4e72f8f9358724988c7905a5929faa1
MiltonMullins <miltonmullins92@gmail.com>
2025-02-22 12:54:53 -0300

    fix db close early

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
+	_ "github.com/lib/pq" // postgres driver
 
 	"github.com/miltonmullins/api-rest-go/controllers"
 	"github.com/miltonmullins/api-rest-go/repositories"
@@ -10,7 +13,24 @@ import (
 )
 
 func main() {
-	router := initializeRoutes() // configure routes
+
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	//create the table if not exists
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS people (
+		id SERIAL PRIMARY KEY,
+		name VARCHAR(100) NOT NULL,
+		age INT NOT NULL
+	);`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router := initializeRoutes(db) // configure routes
 
 	server := &http.Server{
 		Addr:    ":8080",
@@ -20,15 +40,17 @@ func main() {
 	server.ListenAndServe() // Run the http server
 }
 
-func initializeRoutes() *http.ServeMux {
+func initializeRoutes(db *sql.DB) *http.ServeMux {
+
+
 
 	controller := controllers.NewControllerPerson(
 		services.NewServicePerson(
-			repositories.NewPersonRepository()))
+			repositories.NewPersonRepository(db)))
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /people", controller.GetAll)
-	mux.HandleFunc("GET /person/{name}", controller.Get)
+	mux.HandleFunc("GET /person/{name}", controller.GetByName)
 	mux.HandleFunc("POST /person", controller.Post)
 	mux.HandleFunc("PUT /person/{name}", controller.Put)
 	mux.HandleFunc("DELETE /person/{name}", controller.Delete)
